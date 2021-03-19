8fa8f3148df2074a65543f3d9f2551a611d4d96f
Md. Rubel Uddin Raihan <raihan.uits123@gmail.com>
2021-03-19 11:41:10 +0600

    Refactored input.jsx

--- src/components/common/input.jsx
+++ src/components/common/input.jsx
@@ -1,19 +1,12 @@
 import React from "react";
 
-const Input = ({ type, name, label, value, error, onChange }) => {
+const Input = ({ name, label, error, ...rest }) => {
   return (
     <div className="form-group">
       <label htmlFor={name} className="form-label">
         {label}
       </label>
-      <input
-        value={value}
-        onChange={onChange}
-        id={name}
-        type={type}
-        name={name}
-        className="form-control"
-      />
+      <input {...rest} id={name} name={name} className="form-control" />
 
       {error && <div className="alert alert-danger">{error}</div>}
     </div>
